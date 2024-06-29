ea0fc9716bbfd9737b97fb78d4b9151474799ef6
Thi Theu Vu <thivu@c3r12s2.42madrid.com>
2024-06-29 11:37:13 +0200

    change variable

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 09:48:13 by thivu             #+#    #+#             */
-/*   Updated: 2024/03/25 10:43:31 by thivu            ###   ########.fr       */
+/*   Updated: 2024/06/29 11:30:26 by thivu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
+	long	num;
+
+	num = n;
+	if (num < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		n = 147483648;
+		num *= -1;
 	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + '0', fd);
+	if (num > 9)
+		ft_putnbr_fd(num / 10, fd);
+	ft_putchar_fd(num % 10 + '0', fd);
 }
+/*
+#include <stdio.h>
+int main(){
+	ft_putnbr_fd(-2147483648, 1);
+	return (0);
+}*/
