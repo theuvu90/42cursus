303523bcfae5052ece24743d4e0ad8d3161fe0b6
Thi Theu Vu <thivu@c1r10s6.42madrid.com>
2024-07-08 10:54:04 +0200

    add printf

---
+++ ft_printf/ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 09:48:13 by thivu             #+#    #+#             */
+/*   Updated: 2024/06/29 11:30:26 by thivu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num *= -1;
+	}
+	if (num > 9)
+		ft_putnbr_fd(num / 10, fd);
+	ft_putchar_fd(num % 10 + '0', fd);
+}
+/*
+#include <stdio.h>
+int main(){
+	ft_putnbr_fd(-2147483648, 1);
+	return (0);
+}*/
