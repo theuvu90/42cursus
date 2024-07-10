54b93e8b03bb6024e252aa9c703586974f68a7ca
Thi Theu Vu <thivu@c1r9s4.42madrid.com>
2024-07-10 10:26:12 +0200

    change header

--- ft_printf/ft_putunsignbr.c
+++ ft_printf/ft_putunsignbr.c
@@ -5,23 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/09 12:15:35 by thivu             #+#    #+#             */
-/*   Updated: 2024/07/09 15:03:17 by thivu            ###   ########.fr       */
+/*   Created: 2024/07/10 10:08:57 by thivu             #+#    #+#             */
+/*   Updated: 2024/07/10 10:24:39 by thivu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int ft_putunsignbr(unsigned int n)
+int	ft_putunsignbr(unsigned int n)
 {
-    unsigned int    count;
+	unsigned int	count;
 
-    count = 0;
-    if (n > 9)
-    {
-        count += ft_putnbr(n / 10);
-        count += ft_putnbr(n % 10);
-    }
-    count += ft_putchar(n + '0');
-    return (count);
+	count = 0;
+	if (n > 9)
+	{
+		count += ft_putnbr(n / 10);
+		count += ft_putnbr(n % 10);
+	}
+	count += ft_putchar(n + '0');
+	return (count);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -5,24 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/08 11:10:46 by thivu             #+#    #+#             */
-/*   Updated: 2024/07/09 15:01:42 by thivu            ###   ########.fr       */
+/*   Created: 2024/07/10 10:09:20 by thivu             #+#    #+#             */
+/*   Updated: 2024/07/10 10:11:54 by thivu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <unistd.h>
 # include <stdarg.h>
 # include <stddef.h>
 # include <limits.h>
 
-int     check_format(const char *format, void *args);
-int	    ft_putchar(char c);
-int	    ft_putstr(char *s);
-int 	ft_putnbr(int n);
-int     ft_putunsignbr(unsigned int n);
+int		check_format(const char *format, void *args);
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+int		ft_putnbr(int n);
+int		ft_putunsignbr(unsigned int n);
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 
