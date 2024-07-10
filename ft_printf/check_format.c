73f3c4068cd89c4e95c5da3079e9af07646c68de
Thi Theu Vu <thivu@c1r9s2.42madrid.com>
2024-07-10 12:20:02 +0200

    add putnbr_base

--- ft_printf/check_format.c
+++ ft_printf/check_format.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   check_format.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thivu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 10:04:46 by thivu             #+#    #+#             */
-/*   Updated: 2024/07/10 10:05:00 by thivu            ###   ########.fr       */
+/*   Updated: 2024/07/10 11:42:48 by thivu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	check_format(const char *format, void *args)
+int	check_format(char format, void *args)
 {
 	int	n;
 
 	n = 0;
-	if (*format == 'c')
+	if (format == 'c')
 		n += ft_putchar((char)args);
-	if (*format == 's')
+	if (format == 's')
 		n += ft_putstr((char *)args);
-	if (*format == 'p')
+	if (format == 'p')
 		n += ft_putpoint((unsigned long)args);
-	if (*format == 'd' || *format == 'i')
+	if (format == 'd' || *format == 'i')
 		n += ft_putnbr((int)args);
-	if (*format == 'u')
+	if (format == 'u')
 		n += ft_putunsignbr((unsigned int)args);
-	if (*format == 'x')
+	if (format == 'x')
 		n += ft_puthex_lower((unsigned int)args);
-	if (*format == 'X')
+	if (format == 'X')
 		n += ft_puthex_upper((unsigned int)args);
 	return (n);
 }
--- ft_printf/ft_putnbr_base.c
+++ ft_printf/ft_putnbr_base.c
@@ -1,15 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 10:06:36 by thivu             #+#    #+#             */
-/*   Updated: 2024/07/10 12:18:47 by thivu            ###   ########.fr       */
+/*   Created: 2024/07/10 11:48:08 by thivu             #+#    #+#             */
+/*   Updated: 2024/07/10 12:18:09 by thivu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-int	ft_printf(char const *format, ...)
-{}
+
+int	ft_putnbr_base(unsigned long nbr, char *base, int result)
+{
+	unsigned long	len;
+
+	len = 0;
+	while (base[len])
+		len++;
+	if (nbr < len)
+	{
+		if (ft_putchar(base[nbr] == -1))
+			return (-1);
+		return (result + 1);
+	}
+	else
+	{
+		result = ft_putnbr_base(nbr / len, base, result);
+		if (result == -1)
+			return (-1);
+		if (ft_putchar(base[nbr % len]) == -1)
+			return (-1);
+		return (result + 1);
+	}
+}
