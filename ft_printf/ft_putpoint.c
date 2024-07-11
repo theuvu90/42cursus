f1f2ecaabacf8599bdc3196399a51f16be867801
theuvu90 <theuvu90.spain@gmail.com>
2024-07-11 11:06:40 +0200

    change ft_putpoint.c

--- ft_printf/ft_putpoint.c
+++ ft_printf/ft_putpoint.c
@@ -6,7 +6,7 @@
 /*   By: thivu <thivu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 10:05:54 by thivu             #+#    #+#             */
-/*   Updated: 2024/07/10 12:52:43 by thivu            ###   ########.fr       */
+/*   Updated: 2024/07/11 10:42:40 by thivu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_putpoint(unsigned long p)
 {
-	if (ft_putstr("0x") == -1 || ft_putnbr_base(p, "0123456789abcdf") == -1)
+	if (ft_putstr("0x") == -1 || ft_putnbr_base(p, "0123456789abcdef") == -1)
 		return (-1);
-	return (ft_putstr("0x") + ft_putnbr_base(p, "0123456789abcdf"));
+	return (ft_putstr("0x") + ft_putnbr_base(p, "0123456789abcdef"));
 }
